f65aa6c1e0abe6a6f8b149c81605c94de1f5882c
EK <eakozyrev09@gmail.com>
2024-06-19 13:32:39 +0700

    format corrected

--- MC_run.py
+++ MC_run.py
@@ -8,47 +8,50 @@ import argparse
 
 
 class RQuant_Stable:
-    '''Class to generate return1 and return10 spectra and its quantiles'''
+    """Class to generate return1 and return10 spectra and its quantiles"""
 
-    def __init__(self,ndays = 750,step = 10,quant_method = 'hazen'):
-        '''
+    def __init__(self, ndays=750, step=10, quant_method="hazen"):
+        """
         self.ndays -- number of return1 values
         self.quant_method -- the method to get quantile, by default = 'hazen'
         self.step -- number of days in the higher order return^k, by default = 10
         self.data1 -- np.array of return1
         self.data10 -- np.array of return^k
-        '''
+        """
         self.ndays = ndays
         self.quant_method = quant_method
         self.step = step
         self.data1 = np.zeros(self.ndays)
-        if self.ndays-self.step + 1 < 1:
+        if self.ndays - self.step + 1 < 1:
             raise Exception("Step have to be <= ndays")
-        self.data10 = np.zeros(self.ndays-self.step + 1)
+        self.data10 = np.zeros(self.ndays - self.step + 1)
 
     def gen_stable(self):
-        '''
+        """
         this method is not used. It is created for validation on scipy.stats.levy_stable \n
-        [http://prac.im.pwr.edu.pl/~hugo/RePEc/wuu/wpaper/HSC_10_05.pdf] 
-        '''
+        [http://prac.im.pwr.edu.pl/~hugo/RePEc/wuu/wpaper/HSC_10_05.pdf]
+        """
         data = np.zeros(self.ndays)
         for i in range(self.ndays):
-            U = np.pi*np.random.random_sample() - np.pi/2.
+            U = np.pi * np.random.random_sample() - np.pi / 2.0
             W = np.random.exponential(1)
-            X = math.sin(1.7*U)/(math.cos(U))**(1./1.7)*(math.cos(U-1.7*U)/W)**(-0.7/1.7)
-            Y = X + 1.
+            X = (
+                math.sin(1.7 * U)
+                / (math.cos(U)) ** (1.0 / 1.7)
+                * (math.cos(U - 1.7 * U) / W) ** (-0.7 / 1.7)
+            )
+            Y = X + 1.0
             data[i] = Y
         return data
-    
+
     def gen_stable_scipy(self):
-        '''
+        """
         run scipy generation of stable distribution with (alpha=1.7,beta=0,loc=1,scale=1);
         self.data1 will be filled by ndays return1 values;
         :return: None.
-        '''
-        gen = scipy.stats.levy_stable(alpha=1.7,beta=0,loc=1,scale=1)
-        self.data1 = gen.rvs(size = self.ndays)
-
+        """
+        gen = scipy.stats.levy_stable(alpha=1.7, beta=0, loc=1, scale=1)
+        self.data1 = gen.rvs(size=self.ndays)
 
     def get_n_from_stable(self):
         """
@@ -56,14 +59,19 @@ class RQuant_Stable:
         :return: None.
         """
         retn = self.data1[0]
-        for i in range(1,self.step):
-            retn = retn*(1+self.data1[i])
+        for i in range(1, self.step):
+            retn = retn * (1 + self.data1[i])
         self.data10[0] = retn
-        for i in range(1,self.data1.size-self.step+1):
-            retn = retn/self.data1[i-1]/(1/self.data1[i]+1)*(1+self.data1[i+self.step-1])
-            self.data10[i] = retn   
-    
-    def fill_single(self,a,quantile):
+        for i in range(1, self.data1.size - self.step + 1):
+            retn = (
+                retn
+                / self.data1[i - 1]
+                / (1 / self.data1[i] + 1)
+                * (1 + self.data1[i + self.step - 1])
+            )
+            self.data10[i] = retn
+
+    def fill_single(self, a, quantile):
         """
         Generate stable spectrum, distribute return10 and calculate quantile;
         :param a: input data to calculate quantile;
@@ -72,9 +80,9 @@ class RQuant_Stable:
         """
         self.gen_stable_scipy()
         self.get_n_from_stable()
-        return np.quantile(a=self.data10,q=quantile,method='hazen')
+        return np.quantile(a=self.data10, q=quantile, method="hazen")
 
-    def get_n_quantile_p(self,nevents,quantile = 0.01):
+    def get_n_quantile_p(self, nevents, quantile=0.01):
         """
         Generate the spectrum of quantiles with nevents of events;
         :param quantile: the required level of quantile, by default = 0.01;
@@ -83,8 +91,8 @@ class RQuant_Stable:
         vectorized_fill_single = np.vectorize(self.fill_single)
         res = np.zeros(shape=nevents)
         return vectorized_fill_single(res, quantile)
-    
-    def get_n_quantile_p_while(self,quantile = 0.01,delta = 0.01):
+
+    def get_n_quantile_p_while(self, quantile=0.01, delta=0.01):
         """
         Generate the spectrum of quantiles untill the mean of the spectrum fluctuates;
         :param quantile: the value of quantile level, by default = 0.01;
@@ -96,22 +104,44 @@ class RQuant_Stable:
         while True:
             self.gen_stable_scipy()
             self.get_n_from_stable()
-            quant = np.quantile(a=self.data10,q=quantile,method='hazen')
-            res = np.append(res,quant)
-            data_mean = np.append(data_mean,res.mean())
+            quant = np.quantile(a=self.data10, q=quantile, method="hazen")
+            res = np.append(res, quant)
+            data_mean = np.append(data_mean, res.mean())
             # stop generation if data_mean[-10:].std() < np.fabs(delta*res.mean())
-            if res.shape[0] > 10 and data_mean[-10:].std() < np.fabs(delta*res.mean()): break
+            if res.shape[0] > 10 and data_mean[-10:].std() < np.fabs(
+                delta * res.mean()
+            ):
+                break
         return res
 
 
-def plot_histogram(ax, data, bins, range, title = '', log=False, histtype='bar', label=None, density=False):
+def plot_histogram(
+    ax,
+    data,
+    bins,
+    range,
+    title="",
+    log=False,
+    histtype="bar",
+    label=None,
+    density=False,
+):
     """
     plot histogramm
     """
-    ax.hist(data, bins=bins, range=range, histtype=histtype, log=log, density=density, label=label)
+    ax.hist(
+        data,
+        bins=bins,
+        range=range,
+        histtype=histtype,
+        log=log,
+        density=density,
+        label=label,
+    )
     ax.set_title(title)
     if label:
         ax.legend()
+
 
 def main(nevent, delta):
     """
@@ -124,41 +154,114 @@ def main(nevent, delta):
 
     # generate and plot return1 spectrum
     data_stable = generator.gen_stable_scipy()
-    data1_range = [np.quantile(generator.data1, 0.01), np.quantile(generator.data1, 0.99)]
-    plot_histogram(ax[0, 0], generator.data1, bins=100, range=data1_range, title='stable distribution of return1', histtype='step')
+    data1_range = [
+        np.quantile(generator.data1, 0.01),
+        np.quantile(generator.data1, 0.99),
+    ]
+    plot_histogram(
+        ax[0, 0],
+        generator.data1,
+        bins=100,
+        range=data1_range,
+        title="stable distribution of return1",
+        histtype="step",
+    )
 
     # generate and plot return10 spectrum
     data_10 = generator.get_n_from_stable()
-    data10_range = [np.quantile(generator.data10, 0.01), np.quantile(generator.data10, 0.99)]
-    plot_histogram(ax[0, 1], generator.data10, bins=500, range=data10_range, title='distribution of return10', log=True)
+    data10_range = [
+        np.quantile(generator.data10, 0.01),
+        np.quantile(generator.data10, 0.99),
+    ]
+    plot_histogram(
+        ax[0, 1],
+        generator.data10,
+        bins=500,
+        range=data10_range,
+        title="distribution of return10",
+        log=True,
+    )
 
     # generate high statistics spectrum of quantiles
     data_q_limit = generator.get_n_quantile_p(nevents=nevent, quantile=0.01)
     q_limit_range = (data_q_limit.min(), data_q_limit.max())
-    plot_histogram(ax[1, 0], data_q_limit, bins=100, range=q_limit_range, log=True, density=True, label=f'n events = {nevent}')
+    plot_histogram(
+        ax[1, 0],
+        data_q_limit,
+        bins=100,
+        range=q_limit_range,
+        log=True,
+        density=True,
+        label=f"n events = {nevent}",
+    )
 
     # generate small sample of events
-    data_q = generator.get_n_quantile_p_while(quantile=0.01,delta=delta)
-    plot_histogram(ax[1, 0], data_q, bins=100, range=q_limit_range, title='1% quantile spectrum in [min,max] range', log=True, histtype='step', density=True, label=f'n events = {data_q.shape[0]}')
+    data_q = generator.get_n_quantile_p_while(quantile=0.01, delta=delta)
+    plot_histogram(
+        ax[1, 0],
+        data_q,
+        bins=100,
+        range=q_limit_range,
+        title="1% quantile spectrum in [min,max] range",
+        log=True,
+        histtype="step",
+        density=True,
+        label=f"n events = {data_q.shape[0]}",
+    )
 
-    q = np.quantile(data_q_limit, 0.01, method='hazen')
-    plot_histogram(ax[1, 1], data_q_limit, bins=100, range=(q, data_q_limit.max()), log=True, density=True, label=f'n events = {nevent}')
-    plot_histogram(ax[1, 1], data_q, bins=100, range=(q, data_q_limit.max()), title='1% quantile spectrum in [1% quantile,max] range', log=True, histtype='step', density=True, label=f'n events = {data_q.shape[0]}')
+    q = np.quantile(data_q_limit, 0.01, method="hazen")
+    plot_histogram(
+        ax[1, 1],
+        data_q_limit,
+        bins=100,
+        range=(q, data_q_limit.max()),
+        log=True,
+        density=True,
+        label=f"n events = {nevent}",
+    )
+    plot_histogram(
+        ax[1, 1],
+        data_q,
+        bins=100,
+        range=(q, data_q_limit.max()),
+        title="1% quantile spectrum in [1% quantile,max] range",
+        log=True,
+        histtype="step",
+        density=True,
+        label=f"n events = {data_q.shape[0]}",
+    )
 
-    print(f'min/max of generated spectrum: {np.round(data_q.min())} {np.round(data_q.max())}')
-    print(f'Number of events = {data_q.shape[0]}')
-    print('Kolmogorov-Smirnov test comparing generated spectrum and high statistics spectrum:')
+    print(
+        f"min/max of generated spectrum: {np.round(data_q.min())} {np.round(data_q.max())}"
+    )
+    print(f"Number of events = {data_q.shape[0]}")
+    print(
+        "Kolmogorov-Smirnov test comparing generated spectrum and high statistics spectrum:"
+    )
     print(stats.ks_2samp(data_q, data_q_limit))
-    
+
     plt.show()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--observations_num', default=4000, required=False, type=int,
-                        help='number of generate quantiles')
-    parser.add_argument('-d', '--stop_generate_at', default=0.01, required=False, type=float,
-                        help='the sign to stop generation')
+    parser.add_argument(
+        "-n",
+        "--observations_num",
+        default=4000,
+        required=False,
+        type=int,
+        help="number of generate quantiles",
+    )
+    parser.add_argument(
+        "-d",
+        "--stop_generate_at",
+        default=0.01,
+        required=False,
+        type=float,
+        help="the sign to stop generation",
+    )
     args = parser.parse_args()
 
     distribution_params = args.observations_num, args.stop_generate_at
-    main(distribution_params[0],distribution_params[1])
+    main(distribution_params[0], distribution_params[1])
